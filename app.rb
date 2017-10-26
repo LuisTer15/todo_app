dc7f42f84f52be9e1ffe3f155416157dbd82de25
LuisTer15 <LuisTer15@github.com>
2017-10-26 11:29:48 +0200

    Añade funcionalidad de completar y eliminar una tarea

--- app.rb
+++ app.rb
@@ -12,11 +12,21 @@ post '/' do
 end
 
 get '/:id/complete' do
+  @task_complete = Tarea.find(params[:id])["title"]
+  erb :complete
+end
+
+patch '/:id/complete' do
   Tarea.update(params[:id])
   redirect '/'
 end
 
 get '/:id/delete' do
+   @task_delete = Tarea.find(params[:id])["title"]
+   erb :delete
+end
+
+delete '/:id/delete' do
   Tarea.destroy(params[:id])
   redirect '/'
 end
